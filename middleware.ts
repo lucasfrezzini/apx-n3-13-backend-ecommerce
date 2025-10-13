ad0eb1f9009b23875cc30af26b9250f2ec1e5c9d
lucasfrezzini <luquiitas.f@gmail.com>
2025-10-13 20:41:28 -0300

    update: CORS authorization

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,14 @@ export function middleware() {
   const response = NextResponse.next();
 
   response.headers.set("Access-Control-Allow-Origin", "*");
-  response.headers.set("Access-Control-Allow-Methods", "GET, OPTIONS");
-  response.headers.set("Access-Control-Allow-Headers", "Content-Type");
+  response.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET, OPTIONS, POST, PUT, DELETE"
+  );
+  response.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
 
   return response;
 }
